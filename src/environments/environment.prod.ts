b859c04c7f389e64548a4c05da5c7d13e28152bb
William Alarcon <williamfernando.alarcon@bbva.com>
2025-03-04 21:18:54 -0500

    se suben variables de ambiente

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,15 @@
+export const environment = {
+  production: true,
+  defaultauth: 'fake-backend',
+  endpointRest: 'http://35.206.84.155:8000',
+  firebaseConfig: {
+    apiKey: '',
+    authDomain: '',
+    databaseURL: '',
+    projectId: '',
+    storageBucket: '',
+    messagingSenderId: '',
+    appId: '',
+    measurementId: ''
+  }
+};
