aff5096398ad1caf9df688e39857c50840bfbca1
William Alarcon <williamfernando.alarcon@bbva.com>
2025-04-30 17:37:37 -0500

    se cambia IP del backend

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: true,
   defaultauth: 'fake-backend',
   //endpointRest: 'http://35.206.84.155:8000',
-  endpointRest: 'http://35.209.132.4:8000',
+  endpointRest: 'http://104.198.254.103',
   firebaseConfig: {
     apiKey: '',
     authDomain: '',
