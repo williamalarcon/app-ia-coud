1cdc9823d57af2d11279b2dbf20344b6d2bdf33d
William Alarcon <williamfernando.alarcon@bbva.com>
2025-05-02 08:49:11 -0500

    se agrega pto

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: true,
   defaultauth: 'fake-backend',
   //endpointRest: 'http://35.206.84.155:8000',
-  endpointRest: 'http://34.8.151.195',
+  endpointRest: 'http://34.8.151.195:8000',
   firebaseConfig: {
     apiKey: '',
     authDomain: '',
